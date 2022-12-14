e85a79305129a81c27c747c5032ed7c5b07f5352
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-12-14 16:34:32 -0500

    allow plugins (#9)

--- lammps/executable.py
+++ lammps/executable.py
@@ -2,9 +2,20 @@
 import os
 import subprocess
 import sys
+try:
+    from importlib import metadata
+except ImportError: # for Python<3.8
+    import importlib_metadata as metadata
 
 
 ROOT_DIR = os.path.dirname(__file__)
+# https://setuptools.readthedocs.io/en/latest/userguide/entry_point.html
+try:
+    eps = metadata.entry_points(group='lammps.plugins')
+except TypeError:
+    eps = metadata.entry_points().get('lammps.plugins', [])
+for ep in eps:
+    plugin = ep.load()
 
 
 def _program(name, args):
