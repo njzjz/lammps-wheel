2e73199d09ffec29705aa42607eebd5710e3c944
Jinzhe Zeng <jinzhe.zeng@ustc.edu.cn>
2025-07-28 02:15:16 +0800

    Update __init__.py

--- lammps/__init__.py
+++ lammps/__init__.py
@@ -11,4 +11,4 @@ from .core import *                     # lgtm [py/polluting-import]
 from .data import *                     # lgtm [py/polluting-import]
 from .pylammps import *                 # lgtm [py/polluting-import]
 
-__version__ = 20240829
+__version__ = 20250722
